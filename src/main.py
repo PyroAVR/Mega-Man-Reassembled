d1bfe80191fdb762d6b287d8b7b3e123328419e2
PyroAVR <itechahm@me.com>
2016-03-06 22:17:33 -0500

    minor fixes/formatting

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 #This is just to get us started, before we get structure worked out.
 import sge
-
 #This implementation detail frustrates programmers from low-level languages...
 from Game import Game
 from Menu import Menu
 
-game_width = 1920
-game_height = 1080
+#defaults
+game_width	=  1280
+game_height	=  720
 
 Game(width= game_width, height= game_height)
 
